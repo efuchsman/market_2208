b72307310aabdc92554611616dd71fcb91d95e59
Eli Fuchsman <efuchsman@gmail.com>
2022-09-20 09:12:31 -0600

    Initialize vendor class

---
+++ spec/vendor_spec.rb
@@ -0,0 +1,15 @@
+require 'rspec'
+require './lib/item'
+require './lib/vendor'
+
+RSpec.describe Vendor do
+  it "exists and has readable attributes" do
+    vendor = Vendor.new("Rocky Mountain Fresh")
+
+    expect(vendor).to be_an_instance_of(Vendor)
+    expect(vendor.name).to eq("Rocky Mountain Fresh")
+    expect(vendor.inventory).to eq({})
+
+  end
+
+end
